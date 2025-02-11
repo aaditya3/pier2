e192319db3b5d95d42edd1432d03e7f234c216c3
Abhimanyu Veer Aditya <abhimanyu.aditya@gmail.com>
2025-02-11 13:22:07 -0800

    Added a decorator to handle db connection rollbacks.

--- src/pier2/database.py
+++ src/pier2/database.py
@@ -1,8 +1,33 @@
+import logging
 import yaml
 from .models import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from functools import wraps
+from fastapi import HTTPException, status
 
+logger = logging.getLogger(__name__)
+
+def transactional(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        db = kwargs['db']
+        try:
+            result = func(*args, **kwargs)
+            db.commit()
+            return result
+        except HTTPException as e:
+            db.rollback()
+            logger.error(f"Transaction failed: {e}")
+            raise
+        except Exception as e:
+            db.rollback()
+            logger.error(f"Transaction failed: {e}")
+            raise HTTPException(status_code =
+                                status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                detail="An internal server error occurred")
+            raise
+    return wrapper
 
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
